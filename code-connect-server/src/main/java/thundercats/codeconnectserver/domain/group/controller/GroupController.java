2394638ff7f906dc7e58358c0ca435ee7bfa80da
JasmineCodes-New <jasmine.irvin@ymail.com>
2022-07-21 14:55:34 -0400

    changes

--- code-connect-server/src/main/java/thundercats/codeconnectserver/domain/group/controller/GroupController.java
+++ code-connect-server/src/main/java/thundercats/codeconnectserver/domain/group/controller/GroupController.java
@@ -1,4 +1,16 @@
 package thundercats.codeconnectserver.domain.group.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import thundercats.codeconnectserver.domain.group.service.GroupService;
+
+import java.util.List;
+
 public class GroupController {
+
+    private GroupService groupService;
+    @Autowired
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
 }
