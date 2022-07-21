6065ace0461dfb7eaf2625987d690205eb64b6b0
adesinaroland <adesinaroland@gmail.com>
2022-07-21 12:10:41 -0400

    Updates

--- code-connect-server/src/main/java/thundercats/codeconnectserver/education/Education.java
+++ code-connect-server/src/main/java/thundercats/codeconnectserver/education/Education.java
@@ -1,4 +1,91 @@
 package thundercats.codeconnectserver.education;
 
 public class Education {
+    private String schoolName;
+    private String location;
+    private String startDate;
+    private String endDate;
+    private String fieldOfStudy;
+    private Boolean isGraduated;
+    private Double gradePointAvg;
+
+    public Education(String schoolName, String location, String startDate, String endDate, String fieldOfStudy,
+                     Boolean isGraduated, Double gradePointAvg) {
+        this.schoolName = schoolName;
+        this.location = location;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.fieldOfStudy = fieldOfStudy;
+        this.isGraduated = isGraduated;
+        this.gradePointAvg = gradePointAvg;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getFieldOfStudy() {
+        return fieldOfStudy;
+    }
+
+    public void setFieldOfStudy(String fieldOfStudy) {
+        this.fieldOfStudy = fieldOfStudy;
+    }
+
+    public Boolean getGraduated() {
+        return isGraduated;
+    }
+
+    public void setGraduated(Boolean graduated) {
+        isGraduated = graduated;
+    }
+
+    public Double getGradePointAvg() {
+        return gradePointAvg;
+    }
+
+    public void setGradePointAvg(Double gradePointAvg) {
+        this.gradePointAvg = gradePointAvg;
+    }
+
+    @Override
+    public String toString() {
+        return "Education{" +
+                "schoolName='" + schoolName + '\'' +
+                ", location='" + location + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", fieldOfStudy='" + fieldOfStudy + '\'' +
+                ", isGraduated=" + isGraduated +
+                ", gradePointAvg=" + gradePointAvg +
+                '}';
+    }
 }
--- code-connect-server/src/test/java/thundercats/codeconnectserver/education/EducationTest.java
+++ code-connect-server/src/test/java/thundercats/codeconnectserver/education/EducationTest.java
@@ -1,2 +1,16 @@
-package thundercats.codeconnectserver.education;public class EducationTest {
+package thundercats.codeconnectserver.education;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EducationTest {
+    @Test
+    public void constructorTest01(){
+        Education education = new Education("State University", "City", "1/1/00",
+                "12/1/00", "Computer Science", true, 3.5);
+        String expected = education.toString();
+        String actual = "";
+        Assertions.assertEquals(expected,actual);
+    }
+
 }
