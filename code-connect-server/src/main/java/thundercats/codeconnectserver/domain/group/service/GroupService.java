a9071518271910ba5aa414d83f181657fb6fc706
JasmineCodes-New <jasmine.irvin@ymail.com>
2022-07-22 14:51:28 -0400

    Group Controller & Service updates

--- code-connect-server/src/main/java/thundercats/codeconnectserver/domain/group/service/GroupService.java
+++ code-connect-server/src/main/java/thundercats/codeconnectserver/domain/group/service/GroupService.java
@@ -1,8 +1,16 @@
 package thundercats.codeconnectserver.domain.group.service;
 
 import org.apache.catalina.User;
+import thundercats.codeconnectserver.domain.exceptions.ResourceCreationException;
+import thundercats.codeconnectserver.domain.exceptions.ResourceNotFoundException;
+import thundercats.codeconnectserver.domain.group.model.Group;
 
 import java.util.List;
 
-public class GroupService {
+public interface GroupService {
+    Group create(Group group) throws ResourceCreationException;
+    Group getById(Long id) throws ResourceNotFoundException;
+    Group update(Long id, Group groupDetail) throws ResourceNotFoundException;
+    List<Group> getAllGroups();
+    void delete(Long id) throws ResourceNotFoundException;
 }
