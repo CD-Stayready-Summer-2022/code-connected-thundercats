a3eb0084a3117ae3187efb3e5db9990e9d38b01b
JasmineCodes-New <jasmine.irvin@ymail.com>
2022-07-25 11:18:07 -0400

    Updates

--- code-connect-server/src/main/java/thundercats/codeconnectserver/domain/group/service/GroupServiceImpl.java
+++ code-connect-server/src/main/java/thundercats/codeconnectserver/domain/group/service/GroupServiceImpl.java
@@ -1,4 +1,44 @@
 package thundercats.codeconnectserver.domain.group.service;
 
-public class GroupServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import thundercats.codeconnectserver.domain.exceptions.ResourceCreationException;
+import thundercats.codeconnectserver.domain.exceptions.ResourceNotFoundException;
+import thundercats.codeconnectserver.domain.group.model.Group;
+import thundercats.codeconnectserver.domain.group.repo.GroupRepo;
+
+import java.util.List;
+
+public class GroupServiceImpl implements GroupService{
+
+    private GroupRepo groupRepo;
+
+    @Autowired
+    public GroupServiceImpl(GroupRepo groupRepo) {
+        this.groupRepo = groupRepo;
+    }
+    @Override
+    public Group create(Group group) throws ResourceCreationException {
+        return null;
+    }
+
+    @Override
+    public Group getById(Long id) throws ResourceNotFoundException {
+        return null;
+    }
+
+    @Override
+    public Group update(Long id, Group groupDetail) throws ResourceNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<Group> getAllGroups() {
+        return groupRepo.findAll();
+    }
+
+    @Override
+    public void delete(Long id) throws ResourceNotFoundException {
+        Group group = getById(id);
+        groupRepo.delete(group);
+    }
 }
